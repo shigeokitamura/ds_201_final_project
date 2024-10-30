251b9524e6070dd8cb15d07ebafa6ceeaeb3076f
Aspyn <jeongfree25@gmail.com>
2024-10-30 15:28:22 -0700

    fixing the problem

--- visualization.py
+++ visualization.py
@@ -2,5 +2,9 @@ import matplotlib.pyplot as plt
 
 def monthly_spending_trend(df):
     df[['Year','Month','Day']] = df['Date'].str.split('-', expand= True)
-    spending = df[df['Category'] != 'Income']
-    spending.groupby('Month')['Amount'].plot(kind='line')
+    print(df)
+    spending = df[df['Type'] == 'Expense']
+    spending.groupby('Month')['Amount'].sum().plot(kind='line')
+    plt.xlabel('month')
+    plt.ylabel('spending')
+    plt.show()
